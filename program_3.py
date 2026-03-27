e5172456c9e99ac974bb343131368d9fbe7ef8b4
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-03-27 11:31:51 -0500

    updated

--- program_3.py
+++ program_3.py
@@ -31,7 +31,7 @@ def sum_numbers_from_file():
 
             if count > 0:
                 #Needed to add commas for the sum otherwise it would keep failing
-                formatted_total = "{:,.2f}".format(total)
+                formatted_total = "{:.2f}".format(total)
                 print(f"Total sum of valid numbers: {formatted_total}")
             else:
                 print("No valid numbers found to sum.")
