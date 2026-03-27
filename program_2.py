9f8556ca548e9e197a84cae513c6a31965c12a7a
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-03-27 09:38:18 -0500

    updated

--- program_2.py
+++ program_2.py
@@ -3,3 +3,36 @@
 # Each random number should be in the range of 1 through 500. 
 # The application should let the user specify how many random numbers the file will hold 
 # (up to 1000).
+
+#Author: Sam Gaines
+#Date: 3/26/2026
+#Title: Random Number File Writer
+
+import random   #imports random numbers
+def random_numbers_to_file():
+    while True:
+        try:                    # Asks user how many numbers up to 1000
+            num_numbers= int(input("How many numbers would you like(up to 1000)? : "))
+            if 1 <= num_numbers <= 1000:
+                break
+                # Handles any user errors, was not sure if I was supposed to add this.
+            print("Please enter a number between 1 and 1000")
+        except ValueError:
+            print("Please enter a number that is valid between 1 and 1000")
+
+    with open('random_numbers.txt', 'w') as file:
+        for _ in range(num_numbers):
+            random_number = random.randint(1,500)
+            file.write(str(random_number) + '\n')
+
+    print(f" {num_numbers} random numbers where writen as instructed ")
+
+if __name__ == '__main__':
+    random_numbers_to_file()
+
+
+
+
+
+
+
--- program_3.py
+++ program_3.py
@@ -1,16 +1,35 @@
 # Program #3: Average Numbers
 # Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk.
 # (please use the provided numbers.txt)
-# Write a program that reads all of the numbers stored in the file and calculates their total.  
+# Write a program that reads all the numbers stored in the file and calculates their total.
 
 # The program should handle the following exceptions: 
 
 # It should handle any IOError exceptions that are raised.
 # It should handle any ValueError exceptions that are raised when the items that are read from the file 
 # are converted to a number.
+
+#Author: Sam Gaines
+#Date: 3/27/2026
+#Title: Average Numbers
+
 def sum_numbers_from_file():
     ######################
-    # Add your code here #
+    try:
+        with open('names.txt', 'r') as file:
+            total = 0
+            for line in file:
+                try:
+                    total += int(line)
+                except ValueError:   #handles ValueError
+                    print(f"{line} is not a valid number please convert to a valid number ")
+                    return
+            print(f"{total} numbers added to the file")
+            # This handles the IOError
+    except IOError:
+        print("Error reading the file.")
+    except Exception as e:
+        print(f"Sorry a error had occurred {e}")
     ######################
     print('In the sum_numbers_from_file function')
 
